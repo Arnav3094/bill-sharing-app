170b56c35aa8357d6026276365e7b1b9ff0679e7
ishaan-technogeek <ishaanpgupta@gmail.com>
2024-07-04 19:11:08 +0530

    changed connection to a Connector class

--- models/connection.py
+++ models/connection.py
@@ -1,72 +1,51 @@
 import mysql.connector
 
+class Connector:
+    def __init__(self):
+        self.db_config = self.get_db_config()
+        self.connection = self.get_db_connection()
+        self.cursor = self.connection.cursor(dictionary=True)
 
-def get_db_config():
-    return {
-        # Replace with your actual database configuration
-        'user': 'root',
-        'password': 'root@123',
-        'host': 'localhost',
-        'port': '3306',
-        'database': 'Bill_Sharing_App'
-    }
+    def get_db_config(self):
+        return {
+            # Replace with your actual database configuration
+            'user': 'root',
+            'password': 'root@123',
+            'host': 'localhost',
+            'port': '3306',
+            'database': 'Bill_Sharing_App'
+        }
 
+    def get_db_connection(self):
+        try:
+            return mysql.connector.connect(**self.db_config)
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return None
 
-def get_db_connection():
-    config = get_db_config()
+    def close(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.connection:
+            self.connection.close()
 
-    try:
-        db = mysql.connector.connect(**config)
-        return db
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        return None
+    def execute_query(self, query, params=None, fetchall=False):
+        try:
+            self.cursor.execute(query, params)
+            if fetchall:
+                return self.cursor.fetchall()
+            else:
+                return self.cursor.fetchone()
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return None
 
-
-def commit_and_close(db, cursor):
-    try:
-        db.commit()
-    except mysql.connector.Error as err:
-        print(f"Commit Error: {err}")
-    finally:
-        cursor.close()
-        close_db_connection(db)
-
-
-def close_db_connection(db):
-    if db:
-        db.close()
-
-
-def execute_query(query, params = None, fetchall = False):
-    db = get_db_connection()
-    cursor = db.cursor(dictionary = True)
-    try:
-        cursor.execute(query, params)
-        if fetchall:
-            result = cursor.fetchall()
-        else:
-            result = cursor.fetchone()
-        return result
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        return None
-    finally:
-        cursor.close()
-        close_db_connection(db)
-
-
-def execute_insert(query, params):
-    db = get_db_connection()
-    cursor = db.cursor()
-    try:
-        cursor.execute(query, params)
-        db.commit()
-        return cursor.lastrowid
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        db.rollback()
-        return None
-    finally:
-        cursor.close()
-        close_db_connection(db)
+    def execute_insert(self, query, params):
+        try:
+            self.cursor.execute(query, params)
+            self.connection.commit()
+            return self.cursor.lastrowid
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            self.connection.rollback()
+            return None
